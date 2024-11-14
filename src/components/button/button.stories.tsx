1eb73f27e3af1c5a2e9f7f2669ef0dd81fe5079a
ameen <aladeameen@gmail.com>
2024-11-14 20:40:40 +0000

    add buttons stories

---
+++ src/components/button/button.stories.tsx
@@ -0,0 +1,48 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Button } from './button';
+
+const meta = {
+  title: 'Button',
+  component: Button,
+  args: {
+    children: 'Button',
+    disabled: false,
+    variant: 'primary',
+    sizes: 'medium',
+  },
+  argTypes: {
+    // disabled: {
+    //   control: "boolean",
+    // },
+    variant: {
+      control: 'select',
+    },
+  },
+  tags: ['autodocs'],
+} satisfies Meta;
+
+export default meta;
+type Story = StoryObj<typeof Button>;
+
+export const Primary: Story = {
+  args: {
+    variant: 'primary',
+  },
+};
+export const Secondary: Story = {
+  args: {
+    variant: 'secondary',
+  },
+};
+
+export const Destructive: Story = {
+  args: {
+    variant: 'destructive',
+  },
+};
+
+export const Small: Story = {
+  args: {
+    sizes: 'small',
+  },
+};
--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,3 +1,19 @@
-export const Button = (props: any) => {
-  return <button {...props} />;
+import { ComponentProps } from 'react';
+import clsx from 'clsx';
+import styles from './button.module.css';
+
+export type ButtonProps = ComponentProps<'button'> & {
+  variant?: 'primary' | 'secondary' | 'destructive';
+  sizes?: 'small' | 'medium' | 'large';
+};
+
+export const Button = ({
+  variant = 'primary',
+  sizes = 'medium',
+  className,
+  ...props
+}: ButtonProps) => {
+  const classes = clsx(styles.button, styles[variant], styles[sizes], className);
+
+  return <button className={classes} {...props} />;
 };
